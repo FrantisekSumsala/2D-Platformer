5a9f852ee68fcc0521a7b0bba356e137148ffd8e
FrantisekSumsala <43442115+FrantisekSumsala@users.noreply.github.com>
2021-10-04 23:12:35 +0200

    added smoother level reset transition using a black screen

--- Assets/_2DPlatformer/Scripts/LevelManager.cs
+++ Assets/_2DPlatformer/Scripts/LevelManager.cs
@@ -11,6 +11,15 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private LevelFinish levelFinish;
 
+    [SerializeField]
+    private float resetWaitTime = 1f;
+
+    [SerializeField]
+    private float blackScreenTransitionTime = 1f;
+
+    [SerializeField]
+    private UnityEngine.UI.Image blackScreen;
+
     private void OnEnable()
     {
         levelFinish.OnPlayerFinish += FinishLevel;
@@ -25,11 +34,30 @@ public class LevelManager : MonoBehaviour
 
     private void ResetLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        StartCoroutine(ReloadLevel());
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
     }
 
     private void FinishLevel()
     {
         throw new System.NotImplementedException();
+    }
+
+    private IEnumerator ReloadLevel()
+    {
+        float timer = 0f;
+        while (timer < blackScreenTransitionTime)
+        {
+            timer += Time.deltaTime;
+            Color c = blackScreen.color;
+            c.a = Mathf.Lerp(0, 1, timer / blackScreenTransitionTime);
+            blackScreen.color = c;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(resetWaitTime);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        yield return null;
     }
 }
